0a0ccdff70632dd3b0694628693238e6100155ba
igromanru <igromanru@yahoo.de>
2024-10-20 17:01:10 +0200

    feat: Add CheckAndFixActiveLeyakContainmentID

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -8,9 +8,11 @@
 -- Don't change code below --
 ------------------------------
 ModName = "InfiniteLeyakContainment"
-ModVersion = "1.0.3"
-DebugMode = false
+ModVersion = "1.1.0"
+DebugMode = true
 IsModEnabled = true
+
+local UEHelpers = require("UEHelpers")
 
 local function ModInfoAsPrefix()
     return "["..ModName.." v"..ModVersion.."] "
@@ -18,9 +20,26 @@ end
 
 print(ModInfoAsPrefix().."Starting mod initialization\n")
 
+---@param LeyakContainment ADeployed_LeyakContainment_C
+local function CheckAndFixActiveLeyakContainmentID(LeyakContainment)
+    if not LeyakContainment or not LeyakContainment:IsValid() then return end
+
+    local gameState = UEHelpers.GetGameStateBase() ---@cast gameState AAbiotic_Survival_GameState_C
+    if gameState:IsValid() and gameState.ActiveLeyakContainmentID then
+        local leyakContainmentID = LeyakContainment.SpawnedAssetID:ToString()
+        local activeLeyakContainmentID = gameState.ActiveLeyakContainmentID:ToString()
+        if LeyakContainment.ContainsLeyak == true and activeLeyakContainmentID == "" then
+            gameState['Set Leyak Containment ID'](leyakContainmentID)
+        elseif activeLeyakContainmentID == leyakContainmentID and not LeyakContainment.ContainsLeyak then
+            LeyakContainment:TrapLeyak(0.5)
+        end
+    end
+end
+
 local function IsContainmentCurrentlyActive(Context)
     local leyakContainment = Context:get() ---@type ADeployed_LeyakContainment_C
 
+    CheckAndFixActiveLeyakContainmentID(leyakContainment)
     if IsModEnabled and leyakContainment.ContainsLeyak then
         local stability = leyakContainment['Stability Level']
         local maxStability = leyakContainment.MaxStability
@@ -29,6 +48,7 @@ local function IsContainmentCurrentlyActive(Context)
             print(ModInfoAsPrefix().."[IsContainmentCurrentlyActive]:\n")
             print(ModInfoAsPrefix().."Stability Level: "..stability..'\n')
             print(ModInfoAsPrefix().."MaxStability: "..maxStability..'\n')
+            print(ModInfoAsPrefix().."Difference: "..difference..'\n')
         end
         if difference > 0 then
             -- leyakContainment['Stability Level'] = leyakContainment.MaxStability
